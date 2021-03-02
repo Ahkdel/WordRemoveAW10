a125ec8e21c70fb00b2ba16348a1da0ebdc9e027
Khaled Malhaz <kmalhal@kiversal.com>
2021-03-02 11:06:04 +0100

    Deleted build folders

--- AW10/pkg-config_wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-PKG_CONFIG_SYSROOT_DIR=/opt/fslc-framebuffer/2.5/sysroots/armv7at2hf-neon-fslc-linux-gnueabi
-export PKG_CONFIG_SYSROOT_DIR
-PKG_CONFIG_LIBDIR=/opt/fslc-framebuffer/2.5/sysroots/armv7at2hf-neon-fslc-linux-gnueabi/usr/lib/pkgconfig:/opt/fslc-framebuffer/2.5/sysroots/armv7at2hf-neon-fslc-linux-gnueabi/usr/share/pkgconfig:/opt/fslc-framebuffer/2.5/sysroots/armv7at2hf-neon-fslc-linux-gnueabi/usr/lib/arm-fslc-linux-gnueabi/pkgconfig
-export PKG_CONFIG_LIBDIR
-exec pkg-config "$@"
